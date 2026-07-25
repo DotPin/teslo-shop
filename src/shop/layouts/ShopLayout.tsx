9e58fae78ca818dde01dce676da755198742ef3a
dotpin <d.rojas.asenjo@gmail.com>
2026-07-24 22:35:36 -0400

    FrontEnd Desing Header Home Futter

--- src/shop/layouts/ShopLayout.tsx
+++ src/shop/layouts/ShopLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router'
+import { CustomHeader } from '../components/CustomHeader'
+import { CustomFutter } from '../components/CustomFutter'
 
 export const ShopLayout = () => {
     return (
-        <div>
-            ShopLayout
+        <div className="min-h-screen bg-background">
+            <CustomHeader />
             <Outlet />
+            <CustomFutter />
         </div>
     )
 }
--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,15 +1,10 @@
-import { Button } from '@/components/ui/button'
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron'
 
 export const HomePage = () => {
     return (
-        <div>
-            <h1>Hola mundo</h1>
-            <h1 className="font-montserrat">Hola mundo montserrat</h1>
-            <h1 className="font-montserrat font-thin">Hola mundo montserrat thin</h1>
-            <h1 className="font-montserrat font-normal">Hola mundo montserrat normal</h1>
-            <h1 className="font-montserrat font-bold">Hola mundo montserrat bold</h1>
-            <Button>Hola mundo</Button>
-        </div>
+        <>
+            <CustomJumbotron title='Todos los Productos' />
+        </>
 
     )
 }
