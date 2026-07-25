0999c83fcf5c44ac5e7f1c83a5a6156e090a75eb
dotpin <d.rojas.asenjo@gmail.com>
2026-07-24 22:10:39 -0400

    First desing page

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
 
 export const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div>
+            <h1>Hola mundo</h1>
+            <h1 className="font-montserrat">Hola mundo montserrat</h1>
+            <h1 className="font-montserrat font-thin">Hola mundo montserrat thin</h1>
+            <h1 className="font-montserrat font-normal">Hola mundo montserrat normal</h1>
+            <h1 className="font-montserrat font-bold">Hola mundo montserrat bold</h1>
+            <Button>Hola mundo</Button>
+        </div>
     )
 }
