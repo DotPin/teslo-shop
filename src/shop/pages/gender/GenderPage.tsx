361956f7b009468919968310bb25514ffcc5243c
dotpin <d.rojas.asenjo@gmail.com>
2026-07-30 18:04:56 -0400

    Add new features pages

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,22 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductGrid } from "@/shop/components/ProductGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+  const { gender } = useParams()
+
+  const genderLabel = gender == 'men' ? 'Hombres' : gender == 'women' ? 'Mujeres' : 'Niños';
+
   return (
-    <div>GenderPage</div>
+    <>
+      <CustomJumbotron title={`Productos para ${genderLabel}`} />
+
+      <ProductGrid products={products} />
+
+      <CustomPagination totalPages={4} />
+    </>
   )
 }
