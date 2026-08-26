423d84f711e2827de90b7556bd1c184f48b9b02e
dotpin <d.rojas.asenjo@gmail.com>
2026-08-26 15:14:00 -0400

    Develop auth with API

--- src/TesloShop.tsx
+++ src/TesloShop.tsx
@@ -4,12 +4,15 @@ import { appRouter } from "./app.router"
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import { Toaster } from 'sonner'
+
 const queryClient = new QueryClient();
 
 export const TesloShop = () => {
     return (
 
         <QueryClientProvider client={queryClient}>
+            <Toaster />
             <RouterProvider router={appRouter} />
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
