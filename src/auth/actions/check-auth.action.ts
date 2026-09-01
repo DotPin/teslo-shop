01fa4b933782ca012a9b4c9f21e8a42bc0f54cff
dotpin <d.rojas.asenjo@gmail.com>
2026-09-01 17:51:08 -0400

    Configure auth check-status

---
+++ src/auth/actions/check-auth.action.ts
@@ -0,0 +1,23 @@
+import { tesloApi } from "@/api/tesloApi";
+import type { AuthResponse } from "../interfaces/auth.response";
+
+export const checkAuthAction = async (): Promise<AuthResponse> => {
+    const token = localStorage.getIte('token');
+    if (!token) throw new Error('No token');
+
+    try {
+        const { data } = await tesloApi.get<AuthResponse>('auth/check-status', {
+            //headers: {
+            //    Authorization: `Bared ${token}`
+            //}
+        });
+
+        localStorage.setItem('token', data.token);
+
+        return data;
+    } catch (error) {
+        console.log(error)
+        localStorage.removeItem('token');
+        throw new Error('Token expired or not valid');
+    }
+}
