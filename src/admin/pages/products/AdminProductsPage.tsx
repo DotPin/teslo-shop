7d40be74a49ce14471a57c32aa2503b8aff2a313
dotpin <d.rojas.asenjo@gmail.com>
2026-09-08 17:35:55 -0300

    Add and configure search admin products

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,11 +1,21 @@
 import { AdminTitle } from "@/admin/components/AdminTitle"
+import { CustomFullScreenLoading } from "@/components/custom/CustomFullScreenLoading"
 import { CustomPagination } from "@/components/custom/CustomPagination"
 import { Button } from "@/components/ui/button"
 import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "@/components/ui/table"
-import { PlusIcon } from "lucide-react"
+import { currencyFormatter } from "@/lib/currency-formatter"
+import { useProducts } from "@/shop/hooks/useProducts"
+import { PencilIcon, PlusIcon } from "lucide-react"
 import { Link } from "react-router"
 
 export const AdminProductsPage = () => {
+
+    const { data, isLoading } = useProducts();
+
+    if (isLoading) {
+        return <CustomFullScreenLoading />
+    }
+
     return (
         <>
 
@@ -42,31 +52,45 @@ export const AdminProductsPage = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">1</TableCell>
-                        <TableCell>
-                            <img
-                                src='https://placehold.co/250x250'
-                                alt="product"
-                                className="w-20 h-20 object-cover rounded-md"
-                            />
-                        </TableCell>
-                        <TableCell>Producto 1</TableCell>
-                        <TableCell>$240.00</TableCell>
-                        <TableCell>Categoria 1</TableCell>
-                        <TableCell>100 stock</TableCell>
-                        <TableCell>XS,S,L</TableCell>
-                        <TableCell className="text-right">
-                            <Link to={`/admin/products/t-shirt-teslo`}>
-                                Editar
-                            </Link>
-                        </TableCell>
-                    </TableRow>
+                    {
+                        data?.products.map(product => (
+
+                            <TableRow>
+                                <TableCell className="font-medium">1</TableCell>
+                                <TableCell>
+                                    <img
+                                        src={product.images[0]}
+                                        alt={product.title}
+                                        className="w-20 h-20 object-cover rounded-md"
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <Link to={`/admin/products/${product.id}`}
+                                        className='hover:text-blue-500 underline'
+                                    >
+                                        {product.title}
+                                    </Link>
+                                </TableCell>
+                                <TableCell>{product.title}</TableCell>
+                                <TableCell>${currencyFormatter(product.price)}</TableCell>
+                                <TableCell>{product.gender}</TableCell>
+                                <TableCell>{product.stock} stock</TableCell>
+                                <TableCell>{product.sizes}</TableCell>
+                                <TableCell className="text-right">
+                                    <Link to={`/admin/products/${product.id}`}>
+                                        <PencilIcon
+                                            className="w-4 h-4 text-blue-500"
+                                        />
+                                    </Link>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
                 </TableBody>
             </Table>
 
             <CustomPagination
-                totalPages={10}
+                totalPages={data?.pages || 0}
             />
 
 
