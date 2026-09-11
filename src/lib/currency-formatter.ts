cdaed97ba30610dee3ffb5ec7116ae5c744ea12f
dotpin <d.rojas.asenjo@gmail.com>
2026-09-11 14:30:34 -0300

    Configure new component for admin product page editor

--- src/lib/currency-formatter.ts
+++ src/lib/currency-formatter.ts
@@ -1,7 +1,7 @@
 export const currencyFormatter = (value: number) => {
     return value.toLocaleString('es-ES', {
         style: 'currency',
-        currency: 'EUR',
+        currency: 'CLP',
         minimumFractionDigits: 2,
     })
 }
